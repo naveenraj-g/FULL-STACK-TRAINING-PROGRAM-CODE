23fcb0a1825c63b5b1550d39c05c24e2d67eef42
Naveen Raj <naveenraj.gnr2002@gmail.com>
2025-09-14 08:23:43 +0530

    DAY-1_13-09-2025

---
+++ JavaScript/js-course/script.js
@@ -0,0 +1,119 @@
+///////////////////////////////////////////////////////////////////////////////////
+
+// DAY 1 (14-09-25)
+
+/*
+alert("hello")
+alert("world")
+*/
+
+// values and variables
+/*
+console.log("Hello")
+
+let firstname = 'naveen';
+console.log(firstname);
+
+let age = 23;
+let isAlive = true;
+*/
+
+/////////////////////////////////////////////////////////////////////////
+
+// Data types
+// Number, String, boolean, BigInt, null, undefined
+
+// let a = 23;
+// let javascriptIsFun = "fun";
+// let javascriptisFun = true;
+// let b;
+// console.log(b) // undefined
+// let c = null // null
+
+///////////////////////////////////////////////////////////////////////////////
+
+// Basic operators
+// Math operators
+
+// const now = 2025
+// const ageJohn = now - 1997;
+// console.log(ageJohn)
+
+// const firstName = "John";
+// const lastName = "Martha";
+
+// console.log(firstName + " " + lastName)
+
+///////////////////////////////////////////////////////////////////////////////////
+
+// Assignment operator
+// let x = 10 + 5 // 15
+// console.log(x)
+// x += 10 // x = x + 10
+// x++ // x = x + 1
+// x-- // x = x - 1
+// console.log(x)
+
+/////////////////////////////////////////////////////////////////////////////////////
+
+// Comparison operators (>, <, <=, >=)
+// const ageJohn = 25
+// const ageArun = 25
+// console.log(ageJohn > ageArun)
+// console.log(ageJohn >= ageArun)
+// console.log(ageJohn < ageArun)
+// console.log(ageJohn <= ageArun)
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+// Operator Precedence (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Operator_precedence)
+// const now = 2025;
+// const ageJonas = now - 1991;
+// const ageSarah = now - 2018;
+
+// console.log(now - 1991 > now - 2018);
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+// String and Template Literals
+
+// const firstName = "Naveen";
+// const job = "developer";
+
+// const naveen = "I'm " + firstName + ", a " + (2025 - 1999) + " years old";
+// const a = `I'm ${firstName}, a ${2025 - 1999} year old`
+// console.log(a);
+
+//////////////////////////////////////////////////////////////////////////////////////////////////
+
+// variable assignment types (let, const and var)
+
+// let a = 10;
+// a = 20;
+// const b = 10;
+// var c = 10;
+// c = 50
+// console.log(c)
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Taking Decisions: if / else statements
+// const age = 17;
+
+// if (age >= 18) {
+//     console.log("Eligible")
+// } else {
+//     console.log("Not Eligible")
+// }
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Type conversion
+// const inputYear = "46";
+// console.log(inputYear)
+// console.log(typeof inputYear);
+// console.log(typeof Number(inputYear))
+
+// console.log("10" + 10)
+
+///////////////////////////////////////////////////////////////////////////////////////////////
