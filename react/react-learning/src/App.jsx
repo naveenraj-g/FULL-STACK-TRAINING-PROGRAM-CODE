a76f018197e1edbb4303ec14ea5ba89091f942e2
Naveen Raj <naveenraj.gnr2002@gmail.com>
2025-11-08 21:55:57 +0530

    08-11-2025 started react

---
+++ react/react-learning/src/App.jsx
@@ -0,0 +1,21 @@
+import "./App.css";
+import "./global.css";
+
+function Demo() {
+  return (
+    <>
+      <h1>Hello</h1>
+      <p>World</p>
+    </>
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <Demo />
+    </div>
+  );
+}
+
+export default App;
